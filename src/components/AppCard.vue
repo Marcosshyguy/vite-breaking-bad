19d409507e5f9c5b6f46609d9288b92ef75c03f5
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-11-10 17:16:52 +0100

    card

---
+++ src/components/AppCard.vue
@@ -0,0 +1,30 @@
+<script>
+export default{
+  data(){
+
+  },
+  props:{
+
+  }
+}
+</script>
+
+<template>
+<div class="card_custom">
+    <img src="" alt="">
+    <h5>Name</h5>
+    <p>Tv series</p>
+    <p>DEad or alive?</p>
+</div>
+</template>
+
+<style lang="scss">
+@use "../style/partials/variables.scss" as *;
+
+.card_custom{
+    width: 100%;
+    text-align: center;
+    background-color: $main-bg;
+    color: white;
+}
+</style>
